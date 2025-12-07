4e027c1a49fbcfae52a87c136eeb05e6f58fc2fd
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2025-12-07 01:17:40 -0300

    Corrige problemas de enrutamiento para deployar en gh pages

--- simulador/src/App.tsx
+++ simulador/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Inicio from './Inicio.tsx';
 import Partido from './Partido.tsx';
@@ -9,16 +9,14 @@ import Penales from './PenalesSelector.tsx';
 
 const App: React.FC = () =>  {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/partido" element={<Partido />} />
-        <Route path="/penales" element={<Penales />} />
-        <Route path="/penales/simulacion" element={<Penalties />} />
-        <Route path="/partido/simulacion" element={<Score />} />
-      </Routes>
-    </Router>
-  ) 
+    <Routes>
+      <Route path="/" element={<Inicio />} />
+      <Route path="/partido" element={<Partido />} />
+      <Route path="/penales" element={<Penales />} />
+      <Route path="/penales/simulacion" element={<Penalties />} />
+      <Route path="/partido/simulacion" element={<Score />} />
+    </Routes>
+  ); 
 }
 
 export default App;
--- simulador/src/main.tsx
+++ simulador/src/main.tsx
@@ -1,9 +1,12 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  //<React.StrictMode>
-    <App />
-  //</React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
