76e6b984d67097bd4ca50de73b98aa013f1c4808
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2026-05-23 19:56:25 -0300

    Update

--- simulador/vite.config.ts
+++ simulador/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// allow using process in this config without needing @types/node
+declare const process: { env: Record<string, string | undefined> }
+
 // https://vitejs.dev/config/
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
 export default defineConfig({
   plugins: [react()],
-  base: '/simulador-react/',
+  //base: '/simulador-react/',
+  base: repoName ? `/${repoName}/` : './',
 })
